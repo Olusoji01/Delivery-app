d3c8504224c470163e4a3eb291692e2f2b806c6d
Sunday Ajayi <ajayisunday64@gmail.com>
2023-09-10 22:13:53 +0100

    update

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ window.onscroll = function(ev) {
     if (window.scrollY == 0) {
          document.getElementById("heading").style.backgroundColor = "whitesmoke";         
     }
-    else    
-    {
-        document.getElementById("heading").style.backgroundColor = `$("#heading-scrolled")`; 
+}
+
+function myFunc(){
+    window.open("Homepage.html");
+  }
+
+  function myFunc2(){
+    window.open("Homepage2.html");
+  }
+/*<input id="text1" type="text" placeholder="Enter username or email">
+<input type="button" id="submit" value="Submit" onclick="validate()" />
+<script>
+    function validate() {
+        var field = document.getElementById('text1').value;
+
+        // CHeck if email
+        if (/\@/.test(field)) {
+            // Validate email address
+        }
+        else {
+            // Validate username
+        }
     }
-};
+</script>*/
